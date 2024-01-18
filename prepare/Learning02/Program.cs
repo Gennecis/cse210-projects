777ca56008b322d6e1be79ae21845cacdf91ce9c
pascal <okeziepascal2@gmail.com>
2024-01-18 15:53:11 +0100

    first commit for abstraction tuts

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2027;
+        job1._endYear = 2032;
+        job1.Display();
+        // Console.WriteLine($"first job: {job1.DisplayJob()}");
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2032;
+        job2._endYear = 2040;
+        job2.Display();
+        // Console.WriteLine($"second job: {job2.Display()}");
+
+        Resume myResume = new Resume();
+        myResume._name = "Pascal";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
