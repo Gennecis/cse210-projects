97c9da213dec0b88189de530e5e70187de2e561f
pascal <okeziepascal2@gmail.com>
2024-01-30 08:53:02 +0100

    Encapsulation Learning

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"string: {fraction1.GetFractionString()}");
+        Console.WriteLine($"decimal: {fraction1.GetDecimalValue()}");
+        Console.WriteLine();
+        Fraction fraction2 = new Fraction(3);
+        Console.WriteLine($"string: {fraction2.GetFractionString()}");
+        Console.WriteLine($"decimal: {fraction2.GetDecimalValue()}");
+        Console.WriteLine();
+        Fraction fraction3 = new Fraction(1,3);
+        Console.WriteLine($"string: {fraction3.GetFractionString()}");
+        Console.WriteLine($"decimal: {fraction3.GetDecimalValue()}");
     }
 }
